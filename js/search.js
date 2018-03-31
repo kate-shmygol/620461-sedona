f951d8a1b30006ee824920e1faf619badf53df03
kateshmygol <36864125+kateshmygol@users.noreply.github.com>
2018-03-31 16:08:59 +0300

    Minor changes

--- js/search.js
+++ js/search.js
@@ -6,13 +6,3 @@ open.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
 });
-
-// Карта
-var mapLink = document.querySelector(".map-link-popup");
-var mapPopup = document.querySelector(".modal-map");
-
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
-
